4f0d7d4927fa719ae3ca56cc29d434e0ab4d5222
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-03-20 02:42:03 -0600

    a

--- proyecto-catedra-frontend/entrypoint.sh
+++ proyecto-catedra-frontend/entrypoint.sh
@@ -4,8 +4,15 @@ set -e
 # Si no existe la carpeta node_modules, instala las dependencias
 if [ ! -d "node_modules" ]; then
   echo "node_modules no encontrado. Instalando dependencias..."
+<<<<<<< Updated upstream
   npm install --no-interaction
 fi
 
 # Ejecuta el comando que se pase al contenedor
+=======
+  npm install --prefer-dist --optimize-autoloader
+fi
+
+# Ejecuta el comando que se pase al contenedor (como npm run dev)
+>>>>>>> Stashed changes
 exec "$@"
