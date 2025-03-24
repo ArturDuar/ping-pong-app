728b452dde44a6a80f84f4687c37893603ad5e21
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-03-23 21:04:21 -0600

    ultimos cambios para docker

--- proyecto-catedra-api/entrypoint.sh
+++ proyecto-catedra-api/entrypoint.sh
@@ -7,5 +7,18 @@ if [ ! -d vendor ]; then
   composer install --no-interaction --prefer-dist --optimize-autoloader
 fi
 
+# Esperar a que la base de datos esté lista / aqui podemos ajustar el tiempo de espera
+echo "Esperando a que la base de datos esté lista..."
+sleep 10
+
+# Limpiar la configuración y la caché
+echo "Limpiando configuración y caché..."
+php artisan config:clear
+php artisan cache:clear
+
+# Ejecutar migraciones y seeders de Laravel
+php artisan migrate:fresh --force
+php artisan db:seed --force
+
 # Ejecuta el comando que se pase al contenedor
 exec "$@"
