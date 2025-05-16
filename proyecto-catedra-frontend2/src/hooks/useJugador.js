e684c3e60c7c3ccfed1920ae912ab2abb4cd92f7
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-05-16 04:45:51 -0600

    a

--- proyecto-catedra-frontend2/src/hooks/useJugador.js
+++ proyecto-catedra-frontend2/src/hooks/useJugador.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { jugadorService } from '../services/JugadorService';
 
 export function useJugadores() {
--- proyecto-catedra-frontend2/src/hooks/useTorneo.js
+++ proyecto-catedra-frontend2/src/hooks/useTorneo.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { torneoService } from '../services/TorneoService';
 
 export function useTorneos() {
